917e85c85ad3a4d271e46ca644c1986455ff02df
Zach Davis <zeek@Zachs-MacBook-Pro.local>
2019-02-11 13:53:52 -0600

    Added minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,9 +1,15 @@
 import pygame
-import board.py as b
-import game.py as g
+import board as b
+import game as g
 
 pygame.init()
 
+s_width = 500   # Width of screen
+s_height = 500  # Height of screen
 
+win = pygame.display.set_mode((s_width, s_height))  # Created window to display game
 
+run = True
+while run:
+    pygame.time.delay(50)
 
