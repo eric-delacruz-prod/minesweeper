acaf0060db22620df95039b7f5d75d106790a9fa
Owen Mellema <owenmllm@gmail.com>
2019-02-11 17:30:57 -0600

    Fix indexing error

--- board.py
+++ board.py
@@ -36,7 +36,7 @@ class Board():
         elif y > self.y_size or y < 0:
             raise IndexError(f"The given y coordinate {y} is outside of the bounds of the y-axis (Maximum: {self.y_size-1}).")
         else:
-            return self.field[x+(self.y_size*y)]
+            return self.field[x+(self.x_size*y)]
     
     def setFlagged(self, x, y, status):
         '''
