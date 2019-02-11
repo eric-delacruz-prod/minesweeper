dd54df3070f143aa1d99915fee9b8ad3f119f3c9
Zach Davis <zeek@Zachs-MacBook-Pro.local>
2019-02-11 17:07:51 -0600

    Added an quit function to the game

--- minesweeper.py
+++ minesweeper.py
@@ -13,6 +13,8 @@ NUM_BOMBS = 10 # Number of bombs
 window = pygame.display.set_mode((s_width, s_height))  # Created window to display game
 window.fill((0, 0, 0))
 
+pygame.display.set_caption("Minesweeper") # Sets menu bar title
+
 run = True
 # Main game loop
 while run:
@@ -22,17 +24,32 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    '''
+    Generates at 10x10 board
+    Checks if tile has been "cleared" or "flagged"
+    '''
     x_current = x
     y_current = y
     for i in range(10):
         for j in range(10):
             #if g.getCoordinate(i, j)['cleared']:
             #    pygame.draw.rect(window, (210, 210, 210), (x_current, y_current, 50, 50))
+            #if g.getCoordinate(i, j)['flagged']:
+            #   pygame.draw.rect(window, (40, 135, 200), (x_current, y_current, 50, 50))
+            # Draws circles to be used as flags
+            #   pygame.draw.circle(window, (190, 40, 37), (x_current + 25, y_current + 25), 10, 0)
            # else:
-                pygame.draw.rect(window, (40, 135, 200), (x_current, y_current, 50, 50))
-                x_current += 51
+            pygame.draw.rect(window, (40, 135, 200), (x_current, y_current, 50, 50))
+            x_current += 51
         y_current += 51
         x_current = x
+
+
+    # Ends game if ESC is pressed
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]:
+        run = False
+
 
     pygame.display.update()
 
