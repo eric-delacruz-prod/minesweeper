76b489956f266783eeeceac4882bfe2d2aad86f8
Zach Davis <zeek@Zachs-MacBook-Pro.local>
2019-02-11 15:40:43 -0600

    Increased board size

--- minesweeper.py
+++ minesweeper.py
@@ -6,8 +6,9 @@ pygame.init()
 
 s_width = 700   # Width of screen
 s_height = 700  # Height of screen
-x = 150 # Location of board - x-coord
-y = 150 # Location of board - y-coord
+x = 95 # Location of board - x-coord
+y = 95 # Location of board - y-coord
+NUM_BOMBS = 10 # Number of bombs
 
 window = pygame.display.set_mode((s_width, s_height))  # Created window to display game
 window.fill((0, 0, 0))
@@ -23,14 +24,14 @@ while run:
 
     x_current = x
     y_current = y
-    for i in range(1, 9):
-        for j in range(1, 9):
+    for i in range(10):
+        for j in range(10):
             pygame.draw.rect(window, (255, 255, 255), (x_current, y_current, 50, 50))
             x_current += 51
         y_current += 51
         x_current = x
 
-    # pygame.draw.rect(window, (255, 255, 255), (x, y, 300, 300))
+    #
     pygame.display.update()
 
 pygame.quit()
