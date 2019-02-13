42adda526d484050f12aa8cb661a3000abbb2656
Owen Mellema <owenmllm@gmail.com>
2019-02-13 09:51:45 -0600

    Make minesweeper easily extensible

--- minesweeper.py
+++ minesweeper.py
@@ -4,11 +4,19 @@ import game as g
 import math
 
 #GLOBALS
-s_width = 700   # Width of screen
-s_height = 700  # Height of screen
-x = 95 # Location of board - x-coord
-y = 95 # Location of board - y-coord
-NUM_BOMBS = 10 # Number of bombs
+SCREEN_X = 700   # Width of screen
+SCREEN_Y = 700  # Height of screen
+OFFSET_X = 95 # Location of board - x-coord
+OFFSET_Y = 95 # Location of board - y-coord
+TILES_X = 3 #Number of tiles in the x direction
+TILES_Y = 3 #Number of tiles in the y direction
+GAME_SIZE_X = 500 #The size, in pixels, of the playing area in the x direction
+GAME_SIZE_Y = 500 #The size, in pixels, of the playing area in the y direction
+BORDER = 1 #The size, in pixels, of the border between squares.
+NUM_BOMBS = 1 # Number of bombs
+
+tile_width = (GAME_SIZE_X-(TILES_X*BORDER))/TILES_X
+tile_height = (GAME_SIZE_Y-(TILES_Y*BORDER))/TILES_Y
 
 #HELPER FUNCTIONS
 def convertPygameCoordinates(pygame_x, pygame_y, offset_x, offset_y, width, height):
@@ -32,12 +40,12 @@ def convertPygameCoordinates(pygame_x, pygame_y, offset_x, offset_y, width, heig
 
 #Start Pygame
 pygame.init()
-window = pygame.display.set_mode((s_width, s_height))  # Created window to display game
+window = pygame.display.set_mode((SCREEN_X, SCREEN_Y))  # Created window to display game
 window.fill((0, 0, 0))
 pygame.display.set_caption("Minesweeper") # Sets menu bar title
 
 #Initialize Board
-g.initialize(x, y, NUM_BOMBS)
+g.initialize(TILES_X, TILES_Y, NUM_BOMBS)
 
 run = True
 # Main game loop
@@ -57,32 +65,31 @@ while run:
             elif event.button == 3:
                 right_mouse = True
         if event.type == pygame.MOUSEMOTION:
-            print(convertPygameCoordinates(event.pos[0], event.pos[1], x, y, 50, 50))
+            print(convertPygameCoordinates(event.pos[0], event.pos[1], OFFSET_X, OFFSET_Y, tile_height+BORDER, tile_width+BORDER))
 
     if left_mouse:
-        #Do some crap with cPC and event.pos
+        print("hay")
     elif right_mouse:
-        #Same
+        print("yo")
     '''
     Generates at 10x10 board
     Checks if tile has been "cleared" or "flagged"
     '''
-    x_current = x
-    y_current = y
-    for i in range(10):
-        for j in range(10):
+    x_current = OFFSET_X
+    y_current = OFFSET_Y
+    for i in range(TILES_X):
+        for j in range(TILES_Y):
             if g.getCoordinate(i, j)['cleared']:
-                pygame.draw.rect(window, (210, 210, 210), (x_current, y_current, 50, 50))
+                pygame.draw.rect(window, (210, 210, 210), (x_current, y_current, tile_width, tile_height))
             if g.getCoordinate(i, j)['flagged']:
-                pygame.draw.rect(window, (40, 135, 200), (x_current, y_current, 50, 50))
-            # Draws circles to be used as flags
-                pygame.draw.circle(window, (190, 40, 37), (x_current + 25, y_current + 25), 10, 0)
+                pygame.draw.rect(window, (40, 135, 200), (x_current, y_current, tile_width, tile_height))
+                # Draws circles to be used as flags
+                pygame.draw.circle(window, (190, 40, 37), (x_current + (tile_width/2), y_current + (tile_height/2)), 10, 0)
             else:
-                pygame.draw.rect(window, (40, 135, 200), (x_current, y_current, 50, 50))
-                x_current += 51
-        y_current += 51
-        x_current = x
-
+                pygame.draw.rect(window, (40, 135, 200), (x_current, y_current, tile_width, tile_height))
+                x_current += tile_width + BORDER
+        y_current += tile_height + BORDER
+        x_current = OFFSET_X
     # Ends game if ESC is pressed
     keys = pygame.key.get_pressed()
     if keys[pygame.K_ESCAPE]:
