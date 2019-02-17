58600e23674b4198938714894f39be8bf05ae06d
Gwen Liu <k579l290@ku.edu>
2019-02-16 19:53:13 -0600

    Number of flags cannot exceed number of bombs

--- minesweeper.py
+++ minesweeper.py
@@ -148,8 +148,10 @@ while run:
         if event.type == pygame.MOUSEBUTTONDOWN: #Check for pressing a mouse button.
             if event.button == 1: #1 is left click. We set the left click flag to true.
                 left_mouse = True
-            elif event.button == 3: #3 is right click. (Think Left, Middle, Right)
+            elif (event.button == 3) and (NUM_FLAGS > 0): #3 is right click. (Think Left, Middle, Right)
                 right_mouse = True
+                NUM_FLAGS-=1
+                print(NUM_FLAGS)
         if event.type == pygame.MOUSEMOTION: #Check to see if the mouse has moved.
             x_mouse, y_mouse = convertPygameCoordinates(event.pos[0], event.pos[1], OFFSET_X, OFFSET_Y, tile_width+BORDER, tile_height+BORDER) #This function converts PYGAME coordinates to GRID coordinates. See it's documentation for more...
     
@@ -185,10 +187,10 @@ while run:
                 pygame.draw.rect(window, (40, 135, 200), (x_current, y_current, tile_width, tile_height))
             
             #Draw flag/bomb as a circle. Flags take precedance.
-            if g.getCoordinate(i, j)['flagged']:                
-                center_x = int(x_current+(tile_width/2))
-                center_y = int(y_current+(tile_height/2))
-                pygame.draw.circle(window, (255, 0, 0), (center_x, center_y), int(min(tile_height, tile_width)/4))
+            if g.getCoordinate(i, j)['flagged']:         
+                    center_x = int(x_current+(tile_width/2))
+                    center_y = int(y_current+(tile_height/2))
+                    pygame.draw.circle(window, (255, 0, 0), (center_x, center_y), int(min(tile_height, tile_width)/4))
             elif g.getCoordinate(i, j)['bomb'] and SHOW_BOMBS:
                 center_x = int(x_current+(tile_width/2))
                 center_y = int(y_current+(tile_height/2))
